32f21ec2f63ace45939c4c6d48a4cd97deeb30e4
Heather King <hkingdevelopment@gmail.com>
2021-11-03 15:20:23 +0000

    created models and tables

---
+++ models/book.py
@@ -0,0 +1,7 @@
+class Book:
+    def __init__(self, title, author, publisher, publication_date, id=None):
+        self.title = title
+        self.author = author
+        self.publisher = publisher
+        self.publication_date = publication_date
+        self.id = id
