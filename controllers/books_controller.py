eceed84d5b61450ad8cc8205d3fc87c0056a8ac2
Heather King <hkingdevelopment@gmail.com>
2021-11-03 18:25:34 +0000

    adds routes for new and create
    can now add new books to book list and redirects back to book list once added

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from repositories import book_repository
@@ -10,6 +11,28 @@ books_blueprint = Blueprint("books",__name__)
 def books():
     books = book_repository.select_all()
     return render_template("books/index.html", all_books=books)
+
+
+# NEW - GET /books/new
+@books_blueprint.route("/books/new", methods=['GET'])
+def new_book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors = authors)
+
+# CREATE - POST /books
+@books_blueprint.route("/books", methods=['POST'])
+def create_book():
+    title = request.form["title"]
+    author_id = request.form["author_id"]
+    publisher = request.form["publisher"]
+    publication_date = request.form["publication_date"]
+    author = author_repository.select(author_id)
+
+    book = Book(title, author, publisher, publication_date)
+    book_repository.save(book)
+    return redirect("/books")
+
+
 
 
 # DELETE 
